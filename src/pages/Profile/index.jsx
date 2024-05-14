063e249f9d500515b7d81ffbfc68f42881d9b69c
HimaniJoshi0 <himanijoshi225@gmail.com>
2024-05-14 15:54:55 +0530

    profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { services } from '../../services';
 
 const Profile = () => {
+  const[user, setUser] = useState()
+
+  const getBlogs=async(id)=>{
+    console.log("hello")
+
+    const payload = {
+      user_id: id,  
+    };
+
+    try{
+      const response= await services("get",payload,"blogs")
+      console.log("---response---",response)
+    }
+    catch(err){
+      console.log(err)
+    }
+   
+  }
+
+  useEffect(()=>{
+   const userDetails = localStorage.getItem("user")? JSON.parse(localStorage.getItem("user")) : null;
+   console.log("----userDetails-----", userDetails)
+   if (userDetails) {
+    setUser(userDetails);
+   }
+   getBlogs(userDetails.id)
+  },[])
+
+
   return (
-    <div>Profile</div>
+    <div className='flex justify-center items-center'>
+
+      <div>
+      {user && 
+      <div>
+        <h1>{user.name}</h1>
+        <h1>{user.email}</h1>
+      </div>
+        }
+      </div> 
+    </div>
   )
 }
 
